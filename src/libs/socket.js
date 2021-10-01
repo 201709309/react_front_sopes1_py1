13c187488476f25f81d24af11900f65dbb61894b
201709309 <josesantizo149@gmail.com>
2021-09-30 23:06:15 -0600

    se cambio direccion de socket

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -1,5 +1,7 @@
 import io from 'socket.io-client'
 
-let socket = io('//localhost:3001')
+//let socket = io('//localhost:8080')
+
+let socket = io('https://api-sopes1-py1.ue.r.appspot.com')
 
 export default socket;
